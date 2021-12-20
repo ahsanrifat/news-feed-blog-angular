b44d2831ebc43cff8d1a153fdac9790fe79832e8
Syed Rifat Ahsan <syed.rifat.ahsan@gmail.com>
2021-12-20 17:17:54 +0600

    Added modules and auth component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginModule } from './components/login/login.module';
+import { BlogModule } from './components/blog/blog.module';
+import { NewsfeedModule } from './components/newsfeed/newsfeed.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LoginModule,
+    BlogModule,
+    NewsfeedModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
